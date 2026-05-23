7bcb3c321b01385521453110cb3cd4d4380420ae
kalumba samuel <kalumbasml@gmail.com>
2026-05-23 13:44:50 +0300

    temp

--- flask_school_management/sm/app.py
+++ flask_school_management/sm/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return "hello"
+    return render_template('index.html')
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
